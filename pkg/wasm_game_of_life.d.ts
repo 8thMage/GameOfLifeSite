e57af4343bd455aa3da9a65824b6cf356d79a257
the 8th mage <eee34353@gmail.com>
2021-08-07 01:05:02 +0300

    fucking try

--- pkg/wasm_game_of_life.d.ts
+++ pkg/wasm_game_of_life.d.ts
@@ -39,3 +39,31 @@ export class Universe {
 */
   tick(): void;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_universe_free: (a: number) => void;
+  readonly universe_new: () => number;
+  readonly universe_render: (a: number, b: number) => void;
+  readonly universe_width: (a: number) => number;
+  readonly universe_height: (a: number) => number;
+  readonly universe_cells: (a: number) => number;
+  readonly universe_toggle_cell: (a: number, b: number, c: number) => void;
+  readonly universe_tick: (a: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
