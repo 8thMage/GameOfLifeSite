b2282056712ceb6756defee97a68b3444a51cd76
the 8th mage <eee34353@gmail.com>
2021-08-07 00:08:33 +0300

    site 3

---
+++ www2/wasm_game_of_life.d.ts
@@ -0,0 +1,41 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead,
+  Alive,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+/**
+* @param {number} x
+* @param {number} y
+*/
+  toggle_cell(x: number, y: number): void;
+/**
+*/
+  tick(): void;
+}
