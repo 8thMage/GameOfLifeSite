622a9094a10e9a0c4daf9badc8d764bd33e4a872
the 8th mage <eee34353@gmail.com>
2021-08-07 23:26:30 +0300

    run 9 times

--- www2/index.js
+++ www2/index.js
@@ -1,5 +1,5 @@
 import init, { Universe, Cell } from "../pkg/wasm_game_of_life.js";
-const initOutput = await init("../pkg/wasm_game_of_life_bg.wasm");
+const initOutput = init("../pkg/wasm_game_of_life_bg.wasm");
 // import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
 // import { memory } from "../pkg/wasm_game_of_life_bg.js";
 
