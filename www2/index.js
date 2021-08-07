a56eef27b2c00f59e5e3b2a2cf57e6b10b4440d5
the 8th mage <eee34353@gmail.com>
2021-08-07 15:16:30 +0300

    load js

--- www2/index.js
+++ www2/index.js
@@ -1,5 +1,5 @@
 import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
-import { memory } from "../pkg/wasm_game_of_life_bg.wasm";
+import { memory } from "../pkg/wasm_game_of_life_bg.js";
 
 let animationId = null;
 const CELL_SIZE = 5
