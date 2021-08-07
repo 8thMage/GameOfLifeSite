35497da6cdaf1460fed60bec6395bcaeccc3575f
the 8th mage <eee34353@gmail.com>
2021-08-07 22:15:05 +0300

    return universe

--- www2/index.js
+++ www2/index.js
@@ -1,4 +1,4 @@
-import init from "../pkg/wasm_game_of_life.js";
+import init, {Universe, Cell } from "../pkg/wasm_game_of_life.js";
 const initOutput = init("../pkg/wasm_game_of_life_bg.wasm");
 // import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
 // import { memory } from "../pkg/wasm_game_of_life_bg.js";
@@ -10,9 +10,9 @@ const DEAD_COLOR = "#FFFFFF"
 const Alive_COLOR = "#000000"
 
 const canvas = document.getElementById("game-of-life-canvas");
-const universe = initOutput.universe_new();
-const width = initOutput.universe_width(universe);
-const height = initOutput.universe_height(universe);
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
 canvas.height = (CELL_SIZE + 1) * height + 1
 canvas.width = (CELL_SIZE + 1) * width + 1
 const ctx = canvas.getContext('2d')
