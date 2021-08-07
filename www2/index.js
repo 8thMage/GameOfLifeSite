0fbc3d7745d7505a903415b7bbdd1b381926d8c2
the 8th mage <eee34353@gmail.com>
2021-08-07 21:37:15 +0300

    call init?

--- www2/index.js
+++ www2/index.js
@@ -1,5 +1,7 @@
-import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
-import { memory } from "../pkg/wasm_game_of_life_bg.js";
+import { init } from "../pkg/wasm_game_of_life.js";
+init();
+// import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
+// import { memory } from "../pkg/wasm_game_of_life_bg.js";
 
 let animationId = null;
 const CELL_SIZE = 5
