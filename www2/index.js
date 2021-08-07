413ad392e0c8c38a2b8b163ae416a7c52957a9f2
the 8th mage <eee34353@gmail.com>
2021-08-07 23:00:04 +0300

    fix

--- www2/index.js
+++ www2/index.js
@@ -1,42 +1,42 @@
-import init, {Universe, Cell} from "../pkg/wasm_game_of_life.js";
+import init, { Universe, Cell } from "../pkg/wasm_game_of_life.js";
 const initOutput = await init("../pkg/wasm_game_of_life_bg.wasm");
 // import { Universe, Cell } from "../pkg/wasm_game_of_life.js";
 // import { memory } from "../pkg/wasm_game_of_life_bg.js";
 
 const fps = new class {
     constructor() {
-      this.fps = document.getElementById("fps");
-      this.frames = [];
-      this.lastFrameTimeStamp = performance.now();
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
     }
-  
+
     render() {
-      // Convert the delta time since the last frame render into a measure
-      // of frames per second.
-      const now = performance.now();
-      const delta = now - this.lastFrameTimeStamp;
-      this.lastFrameTimeStamp = now;
-      const fps = 1 / delta * 1000;
-  
-      // Save only the latest 100 timings.
-      this.frames.push(fps);
-      if (this.frames.length > 100) {
-        this.frames.shift();
-      }
-  
-      // Find the max, min, and mean of our 100 latest timings.
-      let min = Infinity;
-      let max = -Infinity;
-      let sum = 0;
-      for (let i = 0; i < this.frames.length; i++) {
-        sum += this.frames[i];
-        min = Math.min(this.frames[i], min);
-        max = Math.max(this.frames[i], max);
-      }
-      let mean = sum / this.frames.length;
-  
-      // Render the statistics.
-      this.fps.textContent = `
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
   Frames per Second:
            latest = ${Math.round(fps)}
   avg of last 100 = ${Math.round(mean)}
@@ -44,7 +44,7 @@ const fps = new class {
   max of last 100 = ${Math.round(max)}
   `.trim();
     }
-  };
+};
 
 let animationId = null;
 const CELL_SIZE = 5
@@ -76,7 +76,7 @@ const pause = () => {
 
 playButton.addEventListener("click", event => {
     if (isPaused()) {
-        play(); 
+        play();
     } else {
         pause();
     }
@@ -105,23 +105,39 @@ const drawCells = () => {
     const cellsPtr = universe.cells();
     const cells = new Uint8Array(initOutput.memory.buffer, cellsPtr, width * height);
     ctx.beginPath()
+    ctx.fillStyle = DEAD_COLOR
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
-            ctx.fillStyle = cells[idx] == Cell.Dead ? DEAD_COLOR : Alive_COLOR
-            ctx.fillRect(
-                col * (CELL_SIZE + 1) + 1,
-                row * (CELL_SIZE + 1) + 1,
-                CELL_SIZE,
-                CELL_SIZE)
+            if (cells[idx] == Cell.Dead) {
+                ctx.fillRect(
+                    col * (CELL_SIZE + 1) + 1,
+                    row * (CELL_SIZE + 1) + 1,
+                    CELL_SIZE,
+                    CELL_SIZE)
+            }
         }
     }
+    ctx.fillStyle = Alive_COLOR
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] == Cell.Alive) {
+                ctx.fillRect(
+                    col * (CELL_SIZE + 1) + 1,
+                    row * (CELL_SIZE + 1) + 1,
+                    CELL_SIZE,
+                    CELL_SIZE)
+            }
+        }
+    }
+    
     ctx.stroke();
 }
 
 canvas.addEventListener("click", event => {
     const boundingRect = canvas.getBoundingClientRect();
-  
+
     const scaleX = canvas.width / boundingRect.width;
     const scaleY = canvas.height / boundingRect.height;
     const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
